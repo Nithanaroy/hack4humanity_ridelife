03bb4da3e237dfe4aafcaaa43b58452f78054d90
Nitin Pasumarthy <nithanaroy@gmail.com>
2014-09-20 18:24:12 -0700

    Added remote heroku on compose.io

--- config/db.js
+++ config/db.js
@@ -1,5 +1,6 @@
 var mongo = require('mongojs');
 // var mongoUri = 'mongodb://pppdipcal1zy/mydb';
-var mongoUri = 'mongodb://localhost/hack4humanity';
+// var mongoUri = 'mongodb://localhost/hack4humanity';
+var mongoUri = 'mongodb://ridelife:saibaba@kahana.mongohq.com:10092/hack4humanity_ridelife';
 
 module.exports = mongo(mongoUri);
